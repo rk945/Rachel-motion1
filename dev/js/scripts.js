9a41aff8d6d8e335e0bfe4be11f0e53919b3d487
Rachel <rk945@drexel.edu>
2022-10-06 11:13:18 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,11 +12,24 @@ function heroAnimation(){
 
 }
 
+let mm = gsap.matchMedia();
+var buttonSize = 1;
+
+mm.add("(min-width: 800px)", () => {
+  // desktop setup code here...
+  buttonSize = 2;
+});
+
+mm.add("(max-width: 799px)", () => {
+  // mobile setup code here...
+  buttonSize = 1.25;
+});
+
 var mainTL = gsap.timeline();
 mainTL.add(heroAnimation());
 
 var buttonAnimation =  gsap.timeline({paused:true});
-buttonAnimation.to("#trail-btn",{duration:0.25, scale:2})
+buttonAnimation.to("#trail-btn",{duration:0.25, scale:buttonSize})
 .to("#trail-btn i",{duration:0.25, rotateY: 180})
 .to("#first-line",{duration:0.25, alpha:0, y:50})
 .to("#second-line",{duration:0.25, alpha:0, y:25});
