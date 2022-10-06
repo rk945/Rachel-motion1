8d932f13a2911201f30ec41b22e32e6781bb5089
Rachel <rk945@drexel.edu>
2022-10-05 23:14:31 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,6 +1,14 @@
 import { gsap } from "gsap";
 
+const tl = gsap.timeline({repeat:-1});
+tl.set("#header2", { color: "repeat([white, white, white, white])" }, 3);	
+tl.from("#header2", { opacity: 0, duration: 1.5});
+tl.to("#header2", { opacity: 0, duration: 1});
+
+
+
 let button = document.querySelector("#blue-btn");
-button.addEventListener("mouseover",function(){
-gsap.to("#blue-btn", {rotation: 360, x: 100, duration: 1});
+
+button.addEventListener("mouseenter",function(){
+gsap.to("#blue-btn", {rotation: 360, z: 100, duration: 1});
 });
