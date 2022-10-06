9795515e804b13fc2efdbd83297f34e277b5e7f3
Rachel <rk945@drexel.edu>
2022-10-06 10:59:47 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,26 +1,34 @@
 import { gsap } from "gsap";
-
 gsap.set("button i",{transformOrigin:"center bottom"});
 
-gsap.from("#first-line",{duration:0.5, alpha:0, y:-100});
-gsap.from("#second-line",{duration:0.5, alpha:0, y:-50, delay:0.25});
-gsap.from("#trail-btn",{duration:0.5, alpha:0, y:50, delay:0.35});
-gsap.from("#trail-btn i",{duration:0.25, alpha:0, rotation:90, delay:0.45});
+function heroAnimation(){
+
+    var tl = gsap.timeline();
+ gsap.from("#first-line",{duration:0.5, alpha:0, y:-100});
+ gsap.from("#second-line",{duration:0.5, alpha:0, y:-50});
+ gsap.from("#trail-btn",{duration:0.5, alpha:0, y:50});
+ gsap.from("#trail-btn i",{duration:0.25, alpha:0, rotation:90});
+ return tl;
+
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(heroAnimation());
+
+var buttonAnimation =  gsap.timeline({paused:true});
+buttonAnimation.to("#trail-btn",{duration:0.25, scale:2})
+.to("#trail-btn i",{duration:0.25, rotateY: 180})
+.to("#first-line",{duration:0.25, alpha:0, y:50})
+.to("#second-line",{duration:0.25, alpha:0, y:25});
+
 
 let button = document.querySelector("#trail-btn");
-button.addEventListener("mouseover",function(){
-    gsap.to("#trail-btn",{duration:0.25, scale:2});
-    gsap.to("#trail-btn i",{duration:0.25, rotateY: 180});
 
-    gsap.to("#first-line",{duration:0.25, alpha:0, y:50});
-    gsap.to("#second-line",{duration:0.25, alpha:0, y:25});
+button.addEventListener("mouseover",function(){
+    buttonAnimation.play();
 
 });
 
 button.addEventListener("mouseout",function(){
-    gsap.to("#trail-btn",{duration:0.25, scale:1});
-    gsap.to("#trail-btn i",{duration:0.25, rotateY: 0});
-
-    gsap.to("#first-line",{duration:0.25, alpha:1, y:0});
-    gsap.to("#second-line",{duration:0.25, alpha:1, y:0});
+    buttonAnimation.reverse();
 });
