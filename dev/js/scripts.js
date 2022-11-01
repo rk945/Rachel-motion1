0b5e278fd71a4cfc78d7738c89d03cbd49d57d72
Rachel <rk945@drexel.edu>
2022-11-01 10:45:58 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,6 +7,16 @@ import { GSDevTools } from "gsap/GSDevTools";
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
+function patternMotion(){
 
+    var tl = gsap.timeline({repeat: -1, repeatDelay:1});
+    tl.to(".odd",{rotation:120, transformOrigin:"center center", strokeDasharray:6, duration:3,  ease: "power4.out"},0)
+    tl.to(".even",{rotation:-120, transformOrigin:"center center", duration:3,  ease: "none"})
+    return tl;
+}
+
+
+var mainTL = gsap.timeline();
+mainTL.add(patternMotion())
 
 GSDevTools.create();
