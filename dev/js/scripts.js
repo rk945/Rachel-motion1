07840cc3cfd2105bcfb216cedd7f65319e9e7c35
Rachel <rk945@drexel.edu>
2022-11-01 10:55:17 -0400

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,8 +10,8 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 function patternMotion(){
 
     var tl = gsap.timeline({repeat: -1, repeatDelay:1});
-    tl.to(".odd",{rotation:120, transformOrigin:"center center", strokeDasharray:6, duration:3,  ease: "power4.out"},0)
-    tl.to(".even",{rotation:-120, transformOrigin:"center center", duration:3,  ease: "none"})
+    tl.from(".odd",{rotation:120, transformOrigin:"center center", duration:3,  ease: "power4.out"},0)
+    .fromTo(".even",{rotation:-120, transformOrigin:"center center", duration:3,  ease: "none"})
     return tl;
 }
 
