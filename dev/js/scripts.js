ac1b7ae8e1a796905d3b2c15d5510c97d8732d26
Rachel <rk945@drexel.edu>
2022-11-22 15:13:39 -0500

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,25 +1,57 @@
 import { gsap } from "gsap";
-
-
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+MorphSVGPlugin.convertToPath("circle, rect");
 
-function basicMotion(){
+function arrowMotion(){
  var tl = gsap.timeline();
- tl.fromTo("arrow",{duration: 0.75, scale:0, y:25, stagger:0.5, transformOrigin:"arrow"},"-=50%")
- tl.fromTo("rectangle",{duration: 0.75, scale:0, y:25, stagger:0.5, transformOrigin:"rectangle"},"-=50%")
- tl.fromTo("square",{duration: 0.75, scale:1, y:0, stagger:0.5, transformOrigin:"square"},"-=50%")
+ tl.to("#arrow",{duration:1, scale:0.75, y:100, transformOrigin:"center"})
  return tl;
 }
+
+
+function rectangleMotion(){
+    var tl = gsap.timeline();
+    tl.to("#rectangle",{duration:0.75, scale:0.5, y:50, transformOrigin:"center"})
+    .to("#rectangle",{duration:0.75, scale:0.5, y:50, fill:"#fff", transformOrigin:"center"})
+    return tl;
+}
+
+function squareMotion(){
+var tl = gsap.timeline();
+tl.to("#square",{duration:0.95, scale:1, y:25, transformOrigin:"center"})
+return tl;
+}
+
+function arrowsquareMotion(){
+    var tl = gsap.timeline();
+    tl.to("#arrow",{duration:0.75, scale:0.65, y:-50, transformOrigin:"center"},"sameTime")
+    .to("#square",{duration:0.75, scale:0.75, y:-25, rotate:45, transformOrigin:"center"},"sameTime")
+    return tl;
+    }
+
+function arrowsquareMorph(){
+    var tl = gsap.timeline();
+    tl.to("#arrow",{duration:1, scale:0.5, x:75, rotate:180, morphSVG:"#triangle"},"sameTime")
+      .to("#square",{duration:1, scale:-0.5, x:-75, y:150, rotate:180, morphSVG:"#circle"},"sameTime")
+    return tl;
+}
+ 
 
 
 
 
 
 var mainTL = gsap.timeline();
-mainTL.add(basicMotion())
+mainTL.add(arrowMotion(),"playTogether")
+.add(rectangleMotion(),"playTogether")
+.add(squareMotion(),"playTogether")
+.add(arrowsquareMotion())
+.add(arrowsquareMorph())
+
+
 
 GSDevTools.create();
