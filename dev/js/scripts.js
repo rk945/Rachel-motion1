80bda32807e5a11316ecbcc56773c16b0c9bf693
Rachel <rk945@drexel.edu>
2022-11-22 09:15:08 -0500

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,10 +7,19 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
+function basicMotion(){
+ var tl = gsap.timeline();
+ tl.fromTo("arrow",{duration: 0.75, scale:0, y:25, stagger:0.5, transformOrigin:"arrow"},"-=50%")
+ tl.fromTo("rectangle",{duration: 0.75, scale:0, y:25, stagger:0.5, transformOrigin:"rectangle"},"-=50%")
+ tl.fromTo("square",{duration: 0.75, scale:1, y:0, stagger:0.5, transformOrigin:"square"},"-=50%")
+ return tl;
+}
 
 
 
 
 
+var mainTL = gsap.timeline();
+mainTL.add(basicMotion())
 
 GSDevTools.create();
