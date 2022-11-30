1b0bf900a482c09f61b1fee3a00330470141226c
Rachel <rk945@drexel.edu>
2022-11-30 12:04:50 -0500

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,15 +9,24 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 MorphSVGPlugin.convertToPath("circle, rect");
 
 
-function scaleMotion(){
-    var tl = gsap.timeline({repeat:-1});
-    tl.to("#wagner-logo",{duration:0.75, scale:1.5, transformOrigin:"center", yoyo:true})
-    .to("#wagner-logo",{duration:0.75, scale:0.1, autoAlpha:0})
-    .to("#wagner-logo",{duration:1, scale:1, autoAlpha:2})
-  return tl;
+function slideMotion(){
+  var tl = gsap.timeline({repeat:-1});
+  tl.from("#big-w",{scale:0, duration:1, stagger:0.5, x: 300, drawSVG: 0})
+  .from("#group",{duration: 0.75, scale:0, y:150, stagger:0.5, transformOrigin:"group"},"-=50%")
+return tl;
 }
 
 
+
+// function slideMotion(){
+//     var tl = gsap.timeline({repeat:-1});
+//     tl.to("#wagner-logo",{duration:0.75, scale:1.5, transformOrigin:"center", yoyo:true})
+//     .to("#wagner-logo",{duration:0.75, scale:0.1, autoAlpha:0})
+//     .to("#wagner-logo",{duration:1, scale:1, autoAlpha:2})
+//   return tl;
+// }
+
+
 var mainTL = gsap.timeline();
-mainTL.add(scaleMotion())
+mainTL.add(slideMotion())
 GSDevTools.create();
