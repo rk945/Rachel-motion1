0d3a69fbc67c5eb3ec5e4843d39567a5502f0048
Rachel <rk945@drexel.edu>
2022-11-30 14:23:28 -0500

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,23 +10,24 @@ MorphSVGPlugin.convertToPath("circle, rect");
 
 
 function slideMotion(){
-  var tl = gsap.timeline({repeat:-1});
+  var tl = gsap.timeline();
   tl.from("#big-w",{scale:0, duration:1, stagger:0.5, x: 300, drawSVG: 0})
-  .from("#group",{duration: 0.75, scale:0, y:150, stagger:0.5, transformOrigin:"group"},"-=50%")
+  .from("#group",{duration:1, scale:0, y:150, stagger:0.5, transformOrigin:"group"},"-=50%")
 return tl;
+}
+
+function scaleMotion (){
+  var tl = gsap.timeline();
+  tl.to("#wagner-logo",{duration:0.5, scale:1.5, transformOrigin:"25% 25%", yoyo:true})
+    .to("#wagner-logo",{duration:0.5, scale:0.1, autoAlpha:0, transformOrigin:"25% 25%"})
+    .to("#wagner-logo",{duration:0.75, scale:1, autoAlpha:2, transformOrigin:"25% 25%"})
+  return tl;
 }
 
 
 
-// function slideMotion(){
-//     var tl = gsap.timeline({repeat:-1});
-//     tl.to("#wagner-logo",{duration:0.75, scale:1.5, transformOrigin:"center", yoyo:true})
-//     .to("#wagner-logo",{duration:0.75, scale:0.1, autoAlpha:0})
-//     .to("#wagner-logo",{duration:1, scale:1, autoAlpha:2})
-//   return tl;
-// }
-
 
 var mainTL = gsap.timeline();
 mainTL.add(slideMotion())
+.add(scaleMotion())
 GSDevTools.create();
